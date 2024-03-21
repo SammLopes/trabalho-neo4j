7e637cb0fdc792e6c968b149445098f1772723b2
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-21 00:11:03 -0300

    Update model Proprietario

--- src/main/java/br/trabalho/trab/model/Proprietario.java
+++ src/main/java/br/trabalho/trab/model/Proprietario.java
@@ -1,5 +1,42 @@
 package bf.trabalho.trab.model;
 
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.schema.Relationship.Direction;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@Node
 public class Proprietario {
+ @Id
+ @GeneratedValue
+ private String id;
+
+ @NonNull
+ private String name;
+
+ @NonNull
+ private int celular;
+
+ @NonNull
+ private String cpf;
+
+ @JsonIgnore
+ @Relationship(type = "E_DONO", direction = Direction.OUTGOING)
+ private List<Moradia> moradias = new ArrayList()<>;
+
+ @JsonIgnore
+ @Relationship(type = "AMOR_ENTERNO", direction = Direction.OUTGOING)
+ private List<Inquilino> inquilinos = new ArrayList()<>;
  
 }
