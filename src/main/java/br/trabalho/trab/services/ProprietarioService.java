64aa31b3d5f0be3f76290a1b847a45623fda9cec
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-30 20:43:10 -0300

    Create ProprietárioService.java

---
+++ src/main/java/br/trabalho/trab/services/ProprietarioService.java
@@ -0,0 +1,19 @@
+package br.trabalho.trab.services;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.trabalho.trab.model.Inquilino;
+import br.trabalho.trab.model.Moradia;
+import br.trabalho.trab.repository.InquilinoRepository;
+
+
+@Service
+public class ProprietarioService{
+
+}
+
