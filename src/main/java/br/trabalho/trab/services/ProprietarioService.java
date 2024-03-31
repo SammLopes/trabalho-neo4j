3c5649f05c5fcafaf2d965e7e99d7eb018c36fc3
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-30 23:34:29 -0300

    Create metodos básicos para PropriedadeService

--- src/main/java/br/trabalho/trab/services/ProprietarioService.java
+++ src/main/java/br/trabalho/trab/services/ProprietarioService.java
@@ -7,13 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.trabalho.trab.model.Inquilino;
+import br.trabalho.trab.model.Proprietario;
 import br.trabalho.trab.model.Moradia;
-import br.trabalho.trab.repository.InquilinoRepository;
+import br.trabalho.trab.repository.ProprietarioRepository;
 
 
 @Service
 public class ProprietarioService{
+
+    private final ProprietarioRepository proprietarioRepository;
+
+    @Autowired
+    public ProprietarioService(ProprietarioRepository proprietarioRepository) {
+
+        this.proprietarioRepository = proprietarioRepository;
+
+    }
+
+    public Proprietario createProprietario(Proprietario p){
+        return null;
+    }
+
+    public Proprietario deleteProprietario(String id){
+        return null;
+    }
+
+    public Proprietario getById(String id){
+        return null;
+    }
+
+    public Proprietario getAll(String id){
+        return null;
+    }
+
+    public Proprietario updateProprietario(Proprietario p){
+        return null;
+    }
+
 
 }
 
