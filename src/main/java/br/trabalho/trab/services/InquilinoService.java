4241ae47954b424a6dc39d6788183350196e7076
MAGOCIZENTO <douglaslessat@gmail.com>
2024-04-05 13:42:34 -0300

    ajustando inquilino

--- src/main/java/br/trabalho/trab/services/InquilinoService.java
+++ src/main/java/br/trabalho/trab/services/InquilinoService.java
@@ -12,15 +12,15 @@ import br.trabalho.trab.repository.InquilinoRepository;
 
 @Service
 public class InquilinoService {
-
+ 
  private final InquilinoRepository inquilinoRepository;
 
- @Autowired
+
  public InquilinoService(InquilinoRepository inquilinoRepository) {
      this.inquilinoRepository = inquilinoRepository;
  }
 
- @Transactional
+ 
  public Inquilino createInquilino(Inquilino inquilino) {
      if (inquilino != null && inquilino.getId() == null) {
          return inquilinoRepository.save(inquilino); // Salva o inquilino no banco de dados
@@ -28,8 +28,8 @@ public class InquilinoService {
          throw new IllegalArgumentException("O Inquilino fornecido é inválido ou já possui um ID.");
      }
  }
+ 
 
- @Transactional
  public void deleteInquilino(Inquilino inquilino) {
      if (inquilino != null && inquilino.getId() != null) {
          inquilinoRepository.delete(inquilino); // Exclui o inquilino do banco de dados
@@ -37,7 +37,8 @@ public class InquilinoService {
          throw new IllegalArgumentException("O Inquilino fornecido é inválido ou não possui um ID.");
      }
  }
- @Transactional
+
+
  public Inquilino updateInquilino (Inquilino inquilino){
   if (inquilino != null && inquilino.getId() != null) {
    Inquilino existingInquilino = inquilinoRepository.findById(inquilino.getId()).orElseThrow(() -> new IllegalArgumentException("O Inquilino fornecido não existe."));
@@ -52,24 +53,19 @@ public class InquilinoService {
   }
  }
 
- @Transactional(readOnly = true)
+
 public Inquilino findInquilinoByName(String name) {
     return inquilinoRepository.findByName(name);
 }
 
- @Transactional(readOnly = true)
-public Inquilino findInquilinoById(String id) {
-    return inquilinoRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Inquilino não encontrado para o ID fornecido: " + id));
-}
 
-@Transactional(readOnly = true)
+
 public List<Inquilino> getAllInquilinos() {
     return inquilinoRepository.findAll();
 }
 
 
-@Transactional(readOnly = true)
+
 public long countInquilinos() {
     return inquilinoRepository.count();
 }
